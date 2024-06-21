4c5f36d0feb80045205e5b8224d08d3c76d7542f
seon <darkness.blooms.sun@gmail.com>
2024-06-21 13:33:26 +0900

    SizeBox 위젯

--- Flutter-chapter05/lib/main.dart
+++ Flutter-chapter05/lib/main.dart
@@ -2,22 +2,16 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(
-    Container(
-      decoration: BoxDecoration(
-        color: Colors.red,
-        border: Border.all(
-          width: 16.0,
-          color: Colors.black,
-        ),
-        borderRadius: BorderRadius.circular(16.0,
-        ),
-
-      ),
+    SizedBox( //일정 크기의 공간을 공백으로 두고 싶을때 사용
       height: 200.0,
-      width: 100.0,
-    )
-  );
+      width: 200.0,
+      //sizeBox는 색상이 없으므로 크기를 확인하는 용도로 Container 추가
+      child: Container(
+        color: Colors.red,
+      ),
+    ));
 }
 
