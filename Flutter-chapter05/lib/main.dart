3b6e2bd804ecb5c8c57f946d69fdebf36f9e1865
seon <darkness.blooms.sun@gmail.com>
2024-06-21 13:36:17 +0900

    Padding 위젯

--- Flutter-chapter05/lib/main.dart
+++ Flutter-chapter05/lib/main.dart
@@ -2,16 +2,23 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
-
+//Padding 예제
+//child 위젯에 여백을 제공할때 사용
 void main() {
   runApp(
-    SizedBox( //일정 크기의 공간을 공백으로 두고 싶을때 사용
-      height: 200.0,
-      width: 200.0,
-      //sizeBox는 색상이 없으므로 크기를 확인하는 용도로 Container 추가
-      child: Container(
-        color: Colors.red,
+    Container(
+      color: Colors.blue,
+      child: Padding(
+        padding: EdgeInsets.all(
+          16.0,
+        ),
+        child: Container(
+          color: Colors.red,
+          width: 50.0,
+          height: 50.0,
+        ),
       ),
-    ));
+    )
+  );
 }
 
