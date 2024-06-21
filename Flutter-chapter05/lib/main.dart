c9f55fd8f865a14bfae32c3aea82087ce1490bbb
seon <darkness.blooms.sun@gmail.com>
2024-06-21 13:41:54 +0900

    Padding_margin 적용  위젯

--- Flutter-chapter05/lib/main.dart
+++ Flutter-chapter05/lib/main.dart
@@ -6,16 +6,21 @@ import 'package:flutter/material.dart';
 //child 위젯에 여백을 제공할때 사용
 void main() {
   runApp(
-    Container(
-      color: Colors.blue,
-      child: Padding(
-        padding: EdgeInsets.all(
-          16.0,
-        ),
-        child: Container(
-          color: Colors.red,
-          width: 50.0,
-          height: 50.0,
+
+    Container(//최상위 검정 컨테이너
+      color: Colors.black,
+      child: Container(//중간 파란 컨테이너
+        color: Colors.blue,
+        //마진 적용 위치
+        margin: EdgeInsets.all(16.0),
+        child: Padding(
+         padding: EdgeInsets.all(16.0),
+          //패딩이 적용된 빨강 컨테이너
+          child: Container(
+            color: Colors.red,
+            width: 50.0,
+            height: 50.0,
+          ),
         ),
       ),
     )
